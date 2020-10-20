693c6524221a5b000ab5f3042e2144b085c7d6ce
Glitch (hello-express) <none>
2020-10-20 19:22:30 +0000

    👛🕷 Checkpoint ./courses.js:8580995/1284

--- courses.js
+++ courses.js
@@ -3,7 +3,30 @@ module.exports = [
     number: 100,
     section: 100,
     name: "",
+    credits: 4,
     prereqs: [],
-    schedule: []
+    days: [],
+    startTime: [],
+    endTime: [],
+  },
+  {
+    number: 181,
+    section: 501,
+    credits: 2,
+    name: "Unity Workshop",
+    prereqs: [],
+    days: ["T"],
+    startTime: [13, 30],
+    endTime: [15, 0]
+  },
+  {
+    number: 205,
+    section: 501,
+    name: "Games Literacy",
+    credits: 4,
+    prereqs: [],
+    days: ["M", "W"],
+    startTime: [15, 10],
+    endTime: [16, 40],
   },
 ];
