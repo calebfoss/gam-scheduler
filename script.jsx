af6239f54557ce3d70150eba5729ab4c601cc5e2
Glitch (hello-express) <none>
2020-10-20 22:08:02 +0000

    👄👙 Checkpoint ./script.jsx:8580995/5469

--- script.jsx
+++ script.jsx
@@ -1,15 +1,33 @@
-/* globals React, ReactDOM */
+/* globals React, ReactDOM, courses */
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { coursesTaken: [] };
   }
-  render {
-    <h1>DePaul Game Design Scheduler</h1>
+  render() {
+    return (
+      <div>
+        <h1>DePaul Game Design Scheduler</h1>
+        <Taken />
+      </div>
+    );
   }
 }
 
-const Taken = props => {};
+const Taken = props => {
+  const allCourses = [];
+  courses.forEach(({ program, number, name, reReqs }) => {
+    if (!allCourses.some(({ name: otherName }) => name === otherName))
+      allCourses.push({ program, number, name });
+  });
+  return (
+    <div>
+      {allCourses.map(({ program, number, name }) => (
+        <p>{name}</p>
+      ))}
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#app"));
