a18a2e3d497ace704d24b4907807dbad17061f72
Glitch (hello-express) <none>
2021-02-16 22:01:42 +0000

    🎽👸 Checkpoint ./springCourses.js:8580995/25137

---
+++ springCourses.js
@@ -0,0 +1,372 @@
+const springCourses = [
+  {
+    program: "GAM",
+    number: 228,
+    section: 620,
+    name: "Ethics In Comp Games/cinema",
+    credits: 4,
+    days: [],
+    startTime: [null, null],
+    endTime: [null, null]
+  },
+  {
+    program: "GAM",
+    number: 240,
+    section: 601,
+    name: "Playgramming",
+    credits: 4,
+    days: ["MW"],
+    startTime: [10, 10],
+    endTime: [11, 40]
+  },
+  {
+    program: "GAM",
+    number: 395,
+    section: 601,
+    name: "Game Development Project Ii",
+    credits: 4,
+    days: ["MW"],
+    startTime: [15, 10],
+    endTime: [16, 40]
+  },
+  {
+    program: "GAM",
+    number: 350,
+    section: 901,
+    name: "Physics For Game Developers",
+    credits: 4,
+    days: ["THUR"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 333,
+    section: 901,
+    name: "The Business Of Games",
+    credits: 4,
+    days: ["WED"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 315,
+    section: 901,
+    name: "Game Sound Design 2",
+    credits: 4,
+    days: ["THUR"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 245,
+    section: 601,
+    name: "Game Development Ii",
+    credits: 4,
+    days: ["MW"],
+    startTime: [11, 50],
+    endTime: [13, 20]
+  },
+  {
+    program: "GAM",
+    number: 365,
+    section: 601,
+    name: "Advanced Game Design",
+    credits: 4,
+    days: ["MW"],
+    startTime: [15, 10],
+    endTime: [16, 40]
+  },
+  {
+    program: "GAM",
+    number: 250,
+    section: 901,
+    name: "Game Sound Design I",
+    credits: 4,
+    days: ["TUE"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 317,
+    section: 901,
+    name: "Game Sound Design And Scoring",
+    credits: 4,
+    days: ["WED"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 231,
+    section: 601,
+    name: "History And Design Of Rpgs",
+    credits: 4,
+    days: ["MW"],
+    startTime: [13, 30],
+    endTime: [15, 0]
+  },
+  {
+    program: "GAM",
+    number: 312,
+    section: 602,
+    name: "Playtesting",
+    credits: 4,
+    days: ["MON"],
+    startTime: [13, 30],
+    endTime: [16, 45]
+  },
+  {
+    program: "GAM",
+    number: 377,
+    section: 601,
+    name: "Game Engine Programming Ii",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [15, 10],
+    endTime: [16, 40]
+  },
+  {
+    program: "GAM",
+    number: 205,
+    section: 601,
+    name: "Games Literacy",
+    credits: 4,
+    days: ["MW"],
+    startTime: [13, 30],
+    endTime: [15, 0]
+  },
+  {
+    program: "GAM",
+    number: 355,
+    section: 601,
+    name: "Solo Game Development Project",
+    credits: 4,
+    days: ["MW"],
+    startTime: [10, 10],
+    endTime: [11, 40]
+  },
+  {
+    program: "GAM",
+    number: 226,
+    section: 601,
+    name: "Fundamentals Of Game Design",
+    credits: 4,
+    days: ["TUE"],
+    startTime: [13, 30],
+    endTime: [16, 45]
+  },
+  {
+    program: "GAM",
+    number: 312,
+    section: 601,
+    name: "Playtesting",
+    credits: 4,
+    days: ["WED"],
+    startTime: [13, 30],
+    endTime: [16, 45]
+  },
+  {
+    program: "GAM",
+    number: 205,
+    section: 301,
+    name: "Games Literacy",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [13, 0],
+    endTime: [14, 30]
+  },
+  {
+    program: "GAM",
+    number: 228,
+    section: 301,
+    name: "Ethics In Comp Games/cinema",
+    credits: 4,
+    days: ["MW"],
+    startTime: [9, 40],
+    endTime: [11, 10]
+  },
+  {
+    program: "GAM",
+    number: 395,
+    section: 901,
+    name: "Game Development Project Ii",
+    credits: 4,
+    days: ["WED"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 224,
+    section: 902,
+    name: "Game Design For Non-majors",
+    credits: 4,
+    days: ["WED"],
+    startTime: [18, 0],
+    endTime: [21, 15]
+  },
+  {
+    program: "GAM",
+    number: 340,
+    section: 601,
+    name: "Practical Scripting For Games",
+    credits: 4,
+    days: ["MW"],
+    startTime: [11, 50],
+    endTime: [13, 20]
+  },
+  {
+    program: "GAM",
+    number: 206,
+    section: 601,
+    name: "History Of Games",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [13, 30],
+    endTime: [15, 0]
+  },
+  {
+    program: "GAM",
+    number: 397,
+    section: 602,
+    name: "Topics In Game Design",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [10, 10],
+    endTime: [11, 40]
+  },
+  {
+    program: "GAM",
+    number: 341,
+    section: 601,
+    name: "Introduction To Level Design",
+    credits: 4,
+    days: ["MW"],
+    startTime: [13, 30],
+    endTime: [15, 0]
+  },
+  {
+    program: "GAM",
+    number: 326,
+    section: 901,
+    name: "Gwap Pre-production",
+    credits: 4,
+    days: ["MON"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 350,
+    section: 910,
+    name: "Physics For Game Developers",
+    credits: 4,
+    days: [],
+    startTime: [null, null],
+    endTime: [null, null]
+  },
+  {
+    program: "GAM",
+    number: 228,
+    section: 302,
+    name: "Ethics In Comp Games/cinema",
+    credits: 4,
+    days: ["MW"],
+    startTime: [11, 20],
+    endTime: [12, 50]
+  },
+  {
+    program: "GAM",
+    number: 244,
+    section: 601,
+    name: "Game Development I",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [13, 30],
+    endTime: [15, 0]
+  },
+  {
+    program: "GAM",
+    number: 224,
+    section: 901,
+    name: "Game Design For Non-majors",
+    credits: 4,
+    days: ["MON"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 397,
+    section: 601,
+    name: "Topics In Game Design",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [11, 50],
+    endTime: [13, 20]
+  },
+  {
+    program: "GAM",
+    number: 392,
+    section: 901,
+    name: "Game Modification Workshop",
+    credits: 4,
+    days: ["TUE"],
+    startTime: [17, 45],
+    endTime: [21, 0]
+  },
+  {
+    program: "GAM",
+    number: 229,
+    section: 601,
+    name: "Presentation And Communication",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [15, 10],
+    endTime: [16, 40]
+  },
+  {
+    program: "GAM",
+    number: 245,
+    section: 602,
+    name: "Game Development Ii",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [11, 50],
+    endTime: [13, 20]
+  },
+  {
+    program: "GAM",
+    number: 228,
+    section: 621,
+    name: "Ethics In Comp Games/cinema",
+    credits: 4,
+    days: [],
+    startTime: [null, null],
+    endTime: [null, null]
+  },
+  {
+    program: "GAM",
+    number: 208,
+    section: 601,
+    name: "Virtual Worlds & Online Commun",
+    credits: 4,
+    days: ["MW"],
+    startTime: [10, 10],
+    endTime: [11, 40]
+  },
+  {
+    program: "GAM",
+    number: 228,
+    section: 601,
+    name: "Ethics In Comp Games/cinema",
+    credits: 4,
+    days: ["TTH"],
+    startTime: [11, 50],
+    endTime: [13, 20]
+  }
+];
